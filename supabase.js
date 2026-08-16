49d25945aadb5fef3473ccc36a360e7f4869a3df
atshreebrand-sketch <atshree.brand@gmail.com>
2026-08-16 19:58:14 +0530

    Add Supabase client configuration

---
+++ supabase.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://schsitbayzsqalkvnpbs.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_rHtZGmayQqlWsI-gJt-i8g_6_7LnaFz";
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
